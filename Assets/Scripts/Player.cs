c3e14aaa72635e42867d66ea40aaffecf53f68e1
Chase <chasew13579@gmail.com>
2025-03-20 09:11:45 -0700

    Player can change directions while moving

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,42 +17,37 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        // Only check for new input if the player is not already moving.
-        if (!isMoving)
+        // Check for directional input and update the moveDirection regardless of current movement.
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
         {
-            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
-            {
-                moveDirection = Vector2.up;
-                isMoving = true;
-            }
-            else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
-            {
-                moveDirection = Vector2.down;
-                isMoving = true;
-            }
-            else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
-            {
-                moveDirection = Vector2.left;
-                isMoving = true;
-            }
-            else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
-            {
-                moveDirection = Vector2.right;
-                isMoving = true;
-            }
+            moveDirection = Vector2.up;
+            isMoving = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            moveDirection = Vector2.down;
+            isMoving = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            moveDirection = Vector2.left;
+            isMoving = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            moveDirection = Vector2.right;
+            isMoving = true;
         }
     }
 
     void FixedUpdate()
     {
-        // If the player is moving, apply constant velocity in the chosen direction.
         if (isMoving)
         {
             rb.linearVelocity = moveDirection * moveSpeed;
         }
     }
 
-    // When colliding with an object tagged "Wall" or "Floor", stop the movement.
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("wall") || collision.gameObject.CompareTag("floor"))
