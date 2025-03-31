8d1fc04d6647e5addac968840e77fb80f3d37271
Jordan Truong <jordantruong26@gmail.com>
2025-03-31 15:51:06 -0700

    final changes

--- Assets/Scripts/Ghost_Scripts/GhostClyde.cs
+++ Assets/Scripts/Ghost_Scripts/GhostClyde.cs
@@ -5,8 +5,6 @@ public class GhostClyde : GhostBehavior
     // Clyde behavior
     [SerializeField] public float retreatDistance = 8f;
 
-    
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         Node node = other.GetComponent<Node>();
